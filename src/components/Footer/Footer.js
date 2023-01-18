fb4c01f7e814f5f924b43c5b6ec7ef956562605a
kasirmalik <102788678+kasirmalik@users.noreply.github.com>
2023-01-18 09:17:01 +0530

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -40,7 +40,7 @@ const Footer = () => {
           <SocialIcons href="https://linkedin.com">
             <AiFillLinkedin size="3rem" />
           </SocialIcons>
-          <SocialIcons href="https://instagram.com">
+          <SocialIcons href="https://www.google.com/search?q=instagram+.com%2Fmalik_kasir&client=ms-android-xiaomi&sxsrf=AJOqlzUHDgYWJ3QZGuyHbYZuBDrQcVM9-Q%3A1674013438943&ei=_mrHY4-EOeGK3LUPwrq7oA4&oq=instagram+.com%2Fmalik_kasir&gs_lcp=ChNtb2JpbGUtZ3dzLXdpei1zZXJwEAMyCAghEKABEMMEOggIABCiBBCwAzoKCCEQoAEQwwQQCkoECEEYAVD9F1iUIGDOJWgAcAB4AIABogKIAaQJkgEDMi01mAEAoAEByAEFwAEB&sclient=mobile-gws-wiz-serp>
             <AiFillInstagram size="3rem" />
           </SocialIcons>
         </SocialContainer>
