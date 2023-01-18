ae25d92754c8aeea57ffabf5fa6b6b7349c8adcc
kasirmalik <102788678+kasirmalik@users.noreply.github.com>
2023-01-18 09:38:26 +0530

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -24,7 +24,7 @@ const Footer = () => {
         </LinkColumn>
         <LinkColumn>
           <LinkTitle>Email</LinkTitle>
-          <LinkItem href="mailto:contact@nasyxnadeem.dev">
+          <LinkItem href="kasirmalik87@gmail.com">
             
           </LinkItem>
         </LinkColumn>
